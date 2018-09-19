2e40fba72b79725852df3e202a28a684b2b15ace
Rami H <rhass99@gmail.com>
2018-09-18 22:12:14 -0700

    trial for heroku

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(process.env.PORT, '127.0.0.1');
+
+console.log(`Server running at http://127.0.0.1:${process.env.PORT}/`);
