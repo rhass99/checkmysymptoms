cf52cb12b0dd581a159696b131e3e390f4fabc66
Rami H <rhass99@gmail.com>
2018-09-21 08:59:46 -0700

    added express server

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 
-import http from 'http';
+import express from 'express'
+const PORT = process.env.Port || 5000
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, '127.0.0.1');
-
-console.log(`Server running at http://127.0.0.1:${process.env.PORT}/`);
+express()
+.get('/', (req, res) => res.send("Hello, world"))
+.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
