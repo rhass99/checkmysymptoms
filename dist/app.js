41efb832de293075a97c9a9ee9088ba9a2120b40
Rami H <rhass99@gmail.com>
2018-09-23 19:31:19 -0700

    removed test file for babel

--- dist/app.js
+++ dist/app.js
@@ -8,9 +8,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 if (process.env.NODE_ENV === 'dev') require('dotenv').config();
 
-var PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-(0, _express2.default)().get('/', function (req, res) {
-  return res.send('Listening to ' + process.env.NODE_ENV + ' on port: ' + PORT);
-}).listen(PORT, '0.0.0.0');
+(0, _express2.default)().get('/', (req, res) => res.send(`Listening to ${process.env.NODE_ENV} on port: ${PORT}`)).listen(PORT, '0.0.0.0');
 //# sourceMappingURL=app.js.map
